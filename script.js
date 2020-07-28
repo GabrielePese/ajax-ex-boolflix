f5d5aaa0c16c2b50f82acb14d9fb817ccfbcacb8
Gabriele <gabrielepese@gmail.com>
2020-07-28 11:35:49 +0200

    problema stelle

--- script.js
+++ script.js
@@ -6,6 +6,8 @@
 
 
 
+// Milestone 2:Trasformiamo il voto da 1 a 10 decimale in un numero intero da 1 a 5, così dapermetterci di stampare a schermo un numero di stelle piene che vanno da 1 a 5,lasciando le restanti vuote (troviamo le icone in FontAwesome).Arrotondiamo sempre per eccesso all’unità successiva, non gestiamo icone mezzepiene (o mezze vuote :P)Trasformiamo poi la stringa statica della lingua in una vera e propria bandiera dellanazione corrispondente, gestendo il caso in cui non abbiamo la bandiera dellanazione ritornata dall’API (le flag non ci sono in FontAwesome).Allarghiamo poi la ricerca anche alle serie tv. Con la stessa azione di ricercadovremo prendere sia i film che corrispondono alla query, sia le serie tv, standoattenti ad avere alla fine dei valori simili (le serie e i film hanno campi nel JSON dirisposta diversi, simili ma non sempre identici)Qui un esempio di chiamata per le serie tv:
+
 function buttonClicked (){
 var targetRicerca = $('#input').val()
 
@@ -21,11 +23,22 @@ var targetRicerca = $('#input').val()
       var compiled = Handlebars.compile(template);
       var target = $("h2");
       target.html("")
+
       for (var i = 0; i < results.length; i++) {
+
         var risulato = results[i]
+        risulato.vote_average = (risulato.vote_average/2)
+        risulato.vote_average = Math.round(risulato.vote_average)
+        console.log(risulato.vote_average);
+
+        for (var i = 0; i < risulato.vote_average; i++) {
+          var targetStella = $('#targetStella')
+          targetStella.append('<i class="fas fa-star"></i>');
+
+        }
+
         var risultatoHTML = compiled(risulato);
         target.append(risultatoHTML)
-
       }
 
     },
